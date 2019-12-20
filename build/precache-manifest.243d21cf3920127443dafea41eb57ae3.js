7507702ad7b534175910c4779e091e6de864c3a8
shuvo habib <shuvohabib@gmail.com>
2019-12-21 00:53:54 +0600

    add build folder

---
+++ build/precache-manifest.243d21cf3920127443dafea41eb57ae3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f743659dee6e0f7ef8cf1c859160e97",
+    "url": "/shiny-event/index.html"
+  },
+  {
+    "revision": "b1fa57a2dc9c849a0e5c",
+    "url": "/shiny-event/static/css/main.6cc37d77.chunk.css"
+  },
+  {
+    "revision": "b002f79bea3114c3da9a",
+    "url": "/shiny-event/static/js/2.d77e92d1.chunk.js"
+  },
+  {
+    "revision": "413434914cd9492a5be5e4ccc2066611",
+    "url": "/shiny-event/static/js/2.d77e92d1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b1fa57a2dc9c849a0e5c",
+    "url": "/shiny-event/static/js/main.9174c295.chunk.js"
+  },
+  {
+    "revision": "8d5c9828cf6473911ead",
+    "url": "/shiny-event/static/js/runtime-main.0f2cb7a5.js"
+  }
+]);
